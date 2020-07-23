d600786e062b56504fa4453870caab92831e2230
mgjeffries <gib.jeffries@gmail.com>
2020-07-23 11:54:39 -0500

    add info and styling

--- scripts/criminals/criminalHTMLConverter.js
+++ scripts/criminals/criminalHTMLConverter.js
@@ -1,7 +1,10 @@
 export const criminalHTML = (criminalObj) => {
   return `
-  <section>
-  ${criminalObj.name}
+  <section class='criminal card'>
+  <p>Criminal Name: ${criminalObj.name}</p>
+  <p>Crime: ${criminalObj.conviction}</p>
+  <p>Term Start:${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
+  <p>Term End:${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
   </section>
   `
 }
