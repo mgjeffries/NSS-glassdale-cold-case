d03a56b4b2c0f2b451f207bc5598819520f1b1a9
mgjeffries <gib.jeffries@gmail.com>
2020-08-13 15:08:35 -0500

    remove reference code

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -48,36 +48,3 @@ const render = ( notesCollection, criminalsCollection ) => {
 const clearNotes = () => {
   contentTarget.innerHTML = ''
 }
-
-/*
-
-
-import { useNotes } from './NoteProvider.js'
-import { useCriminals } from '../criminals/CriminalProvider.js'
-
-
-const render = (noteCollection, criminalCollection) => {
-    contentTarget.innerHTML = noteCollection.map(note => {
-        // Find the related criminal
-        const relatedCriminal = criminalCollection.find(criminal => criminal.id === note.criminalId)
-
-        return `
-            <section class="note">
-                <h2>Note about ${relatedCriminal.name}</h2>
-                ${note.noteText}
-            </section>
-        `
-    })
-}
-
-const NoteList = () => {
-    getNotes()
-        .then(getCriminals)
-        .then(() => {
-            const notes = useNotes()
-            const criminals = useCriminals()
-
-            render(notes, criminals)
-        })
-}
-*/
