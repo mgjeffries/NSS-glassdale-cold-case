5fa6c4c7b034cd82e199c0a08725b42e188950a6
mgjeffries <gib.jeffries@gmail.com>
2020-08-20 11:09:46 -0500

    render facility to dom

--- scripts/criminals/CriminalHTMLConverter.js
+++ scripts/criminals/CriminalHTMLConverter.js
@@ -1,9 +1,19 @@
-export const criminalHTML = (criminalObj) => {
+export const criminalHTML = (criminalObj, facilityArray) => {
   return `
   <section class='criminal card card-id--${criminalObj.id}'>
   <h3>Criminal Profile</h3>
   <p>Criminal Name: ${criminalObj.name}</p>
   <p>Crime: ${criminalObj.conviction}</p>
+  <p>Facilities:</p>
+  <ul>
+    ${
+      facilityArray.map( facility => {
+      return `
+      <li>${facility.facilityName}</li>
+      `
+      }).join("")
+    }
+  </ul>
   <p>Term Start:${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
   <p>Term End:${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
   <button id="associates-button--${criminalObj.id}">Associate Alibis</button>
